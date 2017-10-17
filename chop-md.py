55c29a6fea4bdf87f5d19026e0b4b91714a61fd3
pangara <priya.angara@gov.bc.ca>
2017-10-17 16:03:35 -0700

    Add helpful comments

--- chop-md.py
+++ chop-md.py
@@ -5,11 +5,16 @@ import re
 with open("test.md", "rt") as mdFile:
     LINES = mdFile.read().split("\n")
     print LINES
+    # Regular expressions to match headings, sub headings and sub sub headings
     HEADING_PATTERN = re.compile('^(# )')
     SUBHEADING_PATTERN = re.compile('^(## )')
+    SUBSUBHEADING_PATTERN = re.compile('^(### )')
 
     for lineIndex in range(len(LINES)):
         line = LINES[lineIndex]
+        # If it is the heading,
+        # make a folder with the same name as heading
+        # make a file with the same name as heading
         if HEADING_PATTERN.match(line):
             dirName = line[2:]
             fileName = dirName + ".md"
@@ -19,6 +24,7 @@ with open("test.md", "rt") as mdFile:
                 os.makedirs(dirName)
             lineIndex += 1
             line = LINES[lineIndex]
+            # Iterate over the text in the section description and add it to the new md file
             while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line):
                 mainFile.write(line+ "\n")
                 if lineIndex < len(LINES):
@@ -26,7 +32,7 @@ with open("test.md", "rt") as mdFile:
                     line = LINES[lineIndex]
                 else:
                     break
-
+        # Inside each of the folders make md files for each of the subsections and fill in content
         if SUBHEADING_PATTERN.match(line):
             subFile = open(dirName + "/" + line[3:] + ".md", "w")
             subFile.write("---\n")
@@ -39,6 +45,7 @@ with open("test.md", "rt") as mdFile:
                 line = LINES[lineIndex]
                 while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line):
                     subFile.write(line +"\n")
+
                     lineIndex += 1
                     if lineIndex < len(LINES):
                         line = LINES[lineIndex]
